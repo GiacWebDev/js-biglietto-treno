4720ea7ef75d0a6aed398b8f01a1f020509e376d
GiacWebDev <giacobechini@gmail.com>
2023-09-07 14:29:15 +0200

    [EDIT] js paths guide

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+
+
+// 1. Creare varianti per numeroKilometri e etàPasseggero
+// 2. creare variante per prezzo standart bigliettoKM
+// 3. creare condizionali dove marginare da 0 a 18 anni e dare variante
+// 4. creare condizionali dove marginare da 65 a nolimit e dare variante
+// 5. attribuire alle varianti età lo sconto rispettivo
+
+
+
+
