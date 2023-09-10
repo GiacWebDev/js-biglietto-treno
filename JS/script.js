3f1d7daa6841b2cd503f343daf3a625f91c78658
GiacWebDev <giacobechini@gmail.com>
2023-09-10 19:49:26 +0200

    [EDIT] smarter code transcription

--- JS/script.js
+++ JS/script.js
@@ -14,33 +14,34 @@
 
 
 // 1.
-let numeroKilometri = parseInt(prompt('Quanti kilometri devi effettuare?'));
-let anniPasseggero = parseInt(prompt('Quanti anni hai?'));
+let numeroKilometri = prompt('Quanti kilometri devi effettuare?');
+let anniPasseggero = prompt('Quanti anni hai?');
 
 console.log(numeroKilometri);
 console.log(anniPasseggero);
 
 // 2
-const prezzoBiglietto = 0.21
-// operazione per avere tot biglietto senza sconti in variante costo
-let costo = (prezzoBiglietto * numeroKilometri);
+// prezzodel biglietto per singolo chilometro
+const prezzoBiglietto = 0.21;
 
+// creo variabile per avere tot biglietto standard senza sconti
+let costo = (prezzoBiglietto * numeroKilometri);
 console.log(costo);
 
 // 3
-
+// creo condizionali per sconto su età
 if (anniPasseggero <= 18) {
   costo = (costo - (costo * 20 / 100));
 } else if (anniPasseggero >= 65) {
   costo = (costo - (costo * 40 / 100));
 }
 
+// messaggio che verrà visualizzato
 const messaggio = `Il prezzo del tuo biglietto è di: &euro; ${costo.toFixed(2)}`;
-
 console.log(messaggio);
   
 
 // 4 
-
+// Stampo in pagina mettendo il mio 'messaggio' in html all'interno dell'id 'output'
 document.getElementById('output').innerHTML = messaggio
 
